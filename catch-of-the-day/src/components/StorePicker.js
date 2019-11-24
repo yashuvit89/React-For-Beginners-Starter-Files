5675de547462865439f1da93dd840ff9d6b00608
Yaswanth Pedasanganti <ypedasanganti@atlassian>
2019-11-24 22:20:37 +0530

    Reboot react learning

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { format } from 'path';
+
+export default class StorePicker extends React.Component {
+    render() {
+      return <div className="hello">Hello World</div>
+    }
+  }
